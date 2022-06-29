fc439d0420af67c3c728a983787099446337f097
Robert Anderson <67164106+reanderson89@users.noreply.github.com>
2022-06-29 15:14:37 -0700

    Add files via upload

---
+++ src/test/java/com/roberts_awesome_store/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberts_awesome_store.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
